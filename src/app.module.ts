194ced5c5db49d8cfb1a57a4c5cb38b67fffd133
abdullah <anasircft@gmail.com>
2024-12-11 12:11:57 +0500

    Proper Setup of JWT_SECRET Key

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,21 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { DatabaseModule } from './database/database.module';
-import { AuthModule } from './auth/auth.module';
-import { ConfigModule } from '@nestjs/config';
-import { join } from 'path';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { DatabaseModule } from "./database/database.module";
+import { AuthModule } from "./auth/auth.module";
+import { ConfigModule } from "@nestjs/config";
+import { join } from "path";
 
 @Module({
   imports: [
     DatabaseModule,
     AuthModule,
+    // ConfigModule.forRoot({
+    //   isGlobal: true,
+    // envFilePath: join(__dirname, '../.env'),
+    // }),
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: join(__dirname, '../.env'),
     }),
   ],
   controllers: [AppController],
